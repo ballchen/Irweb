c1ae38e8de418f061be4a486ba2a98738b991ebb
ballchen <kerkerball@gmail.com>
2016-01-18 01:48:12 +0800

    add function and button

--- static/js/app.js
+++ static/js/app.js
@@ -3,4 +3,42 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 	$http.get('/api/pictures/all').success(function(data){
 		$scope.pics = data;
 	})
+
+	$scope.getOther = function(){
+		$http.get('/api/pictures/first?q=other').success(function(data){
+			$scope.pics = data;
+		})
+	}
+	$scope.getSexy = function(){
+		$http.get('/api/pictures/first?q=sexy').success(function(data){
+			$scope.pics = data;
+		})
+
+	}
+	$scope.getLeg = function(){
+		$http.get('/api/pictures/second?q=l').success(function(data){
+			$scope.pics = data;
+		})
+
+	}
+	$scope.getBoob = function(){
+		$http.get('/api/pictures/second?q=b').success(function(data){
+			$scope.pics = data;
+		})
+
+	}
+
+	$scope.getBig = function(){
+		$http.get('/api/pictures/third?q=big').success(function(data){
+			$scope.pics = data;
+		})
+
+	}
+
+	$scope.getSmall = function(){
+		$http.get('/api/pictures/third?q=small').success(function(data){
+			$scope.pics = data;
+		})
+
+	}
 }])
